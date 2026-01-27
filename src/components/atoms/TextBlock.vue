fa2f4323aa623875b12d3bb6ea6545be5a332b86
Jonas Van Kerkhove <jonas.vankerkhove@student.odisee.be>
2026-01-27 21:13:23 +0100

    add component with a title and a text block

---
+++ src/components/atoms/TextBlock.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { textBlock } from '@/types/textBox';
+
+
+const props = defineProps<{textBlock: textBlock}>();
+
+</script>
+
+<template>
+    <div>
+        <h3 v-if="textBlock.title">{{ textBlock.title }}</h3>
+        <p v-if="textBlock.text">{{ textBlock.text }}</p>
+    </div>
+</template>
+<style lang="scss" scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    color: white;
+}
+</style>
