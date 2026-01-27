d5c6b9ffc7f84cbe8b29b97bbe83fd82cec68e32
Jonas Van Kerkhove <jonas.vankerkhove@student.odisee.be>
2026-01-27 21:11:47 +0100

    add an highlighted item component

---
+++ src/components/atoms/HighlightedItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <span>
+    <p><slot></slot></p>
+  </span>
+</template>
+<style lang="scss" scoped>
+    span{
+        background-color: gray;
+        padding: .1rem .6rem;
+        border-radius: .5rem;
+        p{
+            margin: 0;
+            padding: 0;
+            font-size: small;
+            color: white;
+            font-weight: 500;
+            
+        }
+        
+    }
+</style>
