5df78e1fa8c8844e3af001bb135b8124d0776406
Jonas Van Kerkhove <jonas.vankerkhove@student.odisee.be>
2026-01-27 21:12:49 +0100

    add a component to list certain items as highlighted items with a specifick title

---
+++ src/components/molecules/ListView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import HighlightedItem from '../atoms/HighlightedItem.vue';
+
+defineProps(['list'])
+
+</script>
+
+<template>
+    <div>
+        <h2>
+            <slot></slot>
+        </h2>
+        <ul>
+            <li v-for="item in list">
+                <HighlightedItem>{{ item }}</HighlightedItem>
+            </li>
+        </ul>
+    </div>
+</template>
+<style lang="scss" scoped>
+div {
+    display: flex;
+    flex-direction: column;
+
+    h2{
+        color: white;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        padding: 0;
+        margin: 0;
+        gap: .5rem;
+
+        li {
+            display: inherit;
+        }
+    }
+}
+</style>
