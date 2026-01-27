2c26625e98670a9d5b07210479f2675f43d60feb
Jonas Van Kerkhove <jonas.vankerkhove@student.odisee.be>
2026-01-27 21:15:00 +0100

    add a download cv button component

---
+++ src/components/atoms/DownloadButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    
+
+</script>
+
+<template>
+    <a href="/files/cv_Jonas_Van_Kerkhove.pdf" download>
+        <button>Download my cv</button>
+    </a>
+    
+</template>
+<style lang="scss" scoped>
+span {
+    background-color: gray;
+    padding: .1rem .6rem;
+    border-radius: .5rem;
+
+    p {
+        margin: 0;
+        padding: 0;
+        font-size: small;
+        color: white;
+        font-weight: 500;
+
+    }
+
+}
+</style>
