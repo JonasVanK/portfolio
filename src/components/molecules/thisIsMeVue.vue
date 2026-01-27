05069dd595b8c5f6e3dedf4665f389e78a3f3de0
Jonas Van Kerkhove <jonas.vankerkhove@student.odisee.be>
2026-01-27 21:15:22 +0100

    add a this is me component with basic info

---
+++ src/components/molecules/thisIsMeVue.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import DownloadButton from '../atoms/DownloadButton.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <h1>Hey, I am Jonas Van Kerkhove</h1>
+            <p>Software developer from Belgium Be</p>
+            <DownloadButton/>
+        </div>
+        <div>
+            <img src="" alt="this is me">
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
