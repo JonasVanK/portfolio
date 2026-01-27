b5a3370a455ea86c25bbcd1ca9b37228fb8a33ed
Jonas Van Kerkhove <jonas.vankerkhove@student.odisee.be>
2026-01-27 21:14:38 +0100

    add simple component to lo list projects

---
+++ src/components/molecules/ProjectList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { project } from '@/types/project';
+import HighlightedItem from '../atoms/HighlightedItem.vue';
+import { useProjectsStore } from '@/stores/projects';
+
+const projectStore = useProjectsStore();
+
+const projects : project[] = projectStore.projects
+
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="project in projects">
+                <p>{{ project.title }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<style lang="scss" scoped>
+div {
+    display: flex;
+    flex-direction: column;
+
+    h2{
+        color: white;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        padding: 0;
+        margin: 0;
+        gap: .5rem;
+
+        li {
+            display: inherit;
+        }
+    }
+}
+</style>
